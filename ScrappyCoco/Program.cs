1a73140618449b8514a3d1cd866079d8cae8c0ff
Milan Gajic <boromirsrb@yahoo.com>
2026-08-11 14:11:59 +0200

    scrappy coco init

---
+++ ScrappyCoco/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ScrappyCoco;
+using ScrappyCoco.Services.ExchangeRates;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("http://localhost:5022")
+});
+
+
+builder.Services.AddTransient<IExchangeRateService, ExchangeRateService>();
+
+await builder.Build().RunAsync();
