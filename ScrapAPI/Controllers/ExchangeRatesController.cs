29808f496ae80d877712865f921697037979ffcb
Milan Gajic <boromirsrb@yahoo.com>
2026-08-18 15:46:07 +0200

    adds search rates by date

--- ScrapAPI/Controllers/ExchangeRatesController.cs
+++ ScrapAPI/Controllers/ExchangeRatesController.cs
@@ -19,7 +19,7 @@ public class ExchangeRatesController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<PagedList<ExchangeRateDto>>> GetRates([FromQuery] ExchangeRateQueryParams rateQueryParams, CancellationToken cancellationToken)
     {
-        var rates = await _exchangeRateService.GetRatesByCurrencySourceAndTargetId(rateQueryParams.Id, rateQueryParams.TargetCurrencyId, rateQueryParams, cancellationToken);
+        var rates = await _exchangeRateService.GetRatesByCurrencySourceAndTargetId(rateQueryParams.Id, rateQueryParams.TargetCurrencyId, rateQueryParams.Date, rateQueryParams, cancellationToken);
 
         return Ok(rates);
     }
